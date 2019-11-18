159b9bb9f22c57780e3c6a5747aadef6cfbf92b3
Shiv Gogna <sgogna0912@gmail.com>
2019-11-18 09:41:19 -0600

    add auth

--- server/index.js
+++ server/index.js
@@ -1,23 +1,29 @@
-//COMMON JS MODULE
-const express = require('express');
-//APP sets up config
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-//ROUTE HANDLER
-/*
-app: registers this route handler
-get: watch for incoming requests with this method
-    get, post, put, delete, patch
-/: watch for requests trying to access '/'
-req: obj representing incoming request
-res: obj representing outgoing request
-res.send: send back some JSON 
-*/
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-//
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
 const PORT = process.env.PORT || 5000;
-//instructs express to tell Node to listen for traffic on port 5000
 app.listen(PORT);
